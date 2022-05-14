d7626377a1bf2598920e66f79fdc33013cb274a7
Diogenes Garin <diogenes_lg@yahoo.com.br>
2022-05-14 14:23:07 +0200

    test

---
+++ app.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import requests
+
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
+
+
+#1. Let's ask for:
+#- date and time
+
+import datetime
+
+d = st.date_input(
+    "Please select a date",
+    datetime.date(2019, 7, 6))
+t = st.time_input('Please select a time', datetime.time(8, 45))
+
+
+pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
+pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
+dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
+dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
+passenger_count = st.selectbox('passenger count', range(1, 5))
+
+
+#st.write('pickup longitude ', pickup_longitude)
+#st.write('pickup latitude ', pickup_latitude)
+#st.write('date and time:', d,t)
+
+params = {
+    "pickup_datetime": f"{d} {t}",
+    "pickup_longitude": pickup_longitude,
+    "pickup_latitude": pickup_latitude,
+    "dropoff_longitude": dropoff_longitude,
+    "dropoff_latitude": dropoff_latitude,
+    "passenger_count": passenger_count
+    }
+
+response = requests.get(url, params=params)
+response.json()['fare']
